175d27583dd34fc4df2a936a1494f447823fb969
itsujjwall <48892610+itsujjwall@users.noreply.github.com>
2021-05-02 13:16:27 +0530

    JASON parsing is Done Completely

--- app/src/main/java/com/example/covidinfo/MainActivity.java
+++ app/src/main/java/com/example/covidinfo/MainActivity.java
@@ -4,10 +4,16 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,7 +26,16 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.models.PieModel;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     //1
@@ -32,6 +47,12 @@ public class MainActivity extends AppCompatActivity {
     private SwipeRefreshLayout swipeRefreshLayout;
     private PieChart pieChart;
     //4
+    private String str_confirmed, str_confirmed_new, str_active, str_active_new, str_recovered, str_recovered_new,
+            str_death, str_death_new, str_tests, str_tests_new, str_last_update_time;
+    private int int_active_new;
+    //5
+
+    private ProgressDialog progressDialog;
 
 
     @Override
@@ -44,24 +65,111 @@ public class MainActivity extends AppCompatActivity {
 
         //Fetch Data from Api
         FetchData();
+
+
+        swipeRefreshLayout.setOnRefreshListener(() -> {
+            FetchData();
+            swipeRefreshLayout.setRefreshing(false);
+            //Toast.makeText(MainActivity.this, "Data refreshed!", Toast.LENGTH_SHORT).show();
+        });
+
+        lin_state_data.setOnClickListener(v -> {
+            Toast.makeText(this, "State Data Clicked", Toast.LENGTH_SHORT).show();
+        });
+
+        lin_world_data.setOnClickListener(v -> {
+            Toast.makeText(this, "World Data Clicked", Toast.LENGTH_SHORT).show();
+        });
     }
 
 
-
     private void FetchData() {
+
+        //show progress dialog
+        ShowDialog(this);
+
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         String apiUrl = "https://api.covid19india.org/data.json";
 
         pieChart.clearChart();
 
-        JsonObjectRequest jsonObjectRequest= new JsonObjectRequest(
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                 Request.Method.GET,
                 apiUrl,
                 null,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
+                        //As the data of the json are in a nested array, so we need to define the array from which we want to fetch the data.
+                        JSONArray all_state_jsonArray = null;
+                        JSONArray testData_jsonArray = null;
 
+                        try {
+                            all_state_jsonArray = response.getJSONArray("statewise");
+                            testData_jsonArray = response.getJSONArray("tested");//testing is in another array at json
+                            JSONObject data_india = all_state_jsonArray.getJSONObject(0);
+                            JSONObject test_data_india = testData_jsonArray.getJSONObject(testData_jsonArray.length() - 1);
+
+
+                            //Fetching data for India and storing it in String
+                            str_confirmed = data_india.getString("confirmed");   //Confirmed cases in India
+                            str_confirmed_new = data_india.getString("deltaconfirmed");   //New Confirmed cases from last update time
+
+                            str_active = data_india.getString("active");    //Active cases in India
+
+                            str_recovered = data_india.getString("recovered");  //Total recovered cased in India
+                            str_recovered_new = data_india.getString("deltarecovered"); //New recovered cases from last update time
+
+                            str_death = data_india.getString("deaths");     //Total deaths in India
+                            str_death_new = data_india.getString("deltadeaths");    //New death cases from last update time
+
+                            str_last_update_time = data_india.getString("lastupdatedtime"); //Last update date and time
+
+                            str_tests = test_data_india.getString("totalsamplestested"); //Total samples tested in India
+                            str_tests_new = test_data_india.getString("samplereportedtoday");   //New samples tested today
+
+
+                            Handler delayToshowProgess = new Handler();
+
+                            delayToshowProgess.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    //Setting text in the textview
+                                    tv_confirmed.setText(NumberFormat.getInstance().format(Integer.parseInt(str_confirmed)));
+                                    tv_confirmed_new.setText("+" + NumberFormat.getInstance().format(Integer.parseInt(str_confirmed_new)));
+
+                                    tv_active.setText(NumberFormat.getInstance().format(Integer.parseInt(str_active)));
+
+                                    int_active_new = Integer.parseInt(str_confirmed_new)
+                                            - (Integer.parseInt(str_recovered_new) + Integer.parseInt(str_death_new));
+                                    tv_active_new.setText("+" + NumberFormat.getInstance().format(int_active_new));
+
+                                    tv_recovered.setText(NumberFormat.getInstance().format(Integer.parseInt(str_recovered)));
+                                    tv_recovered_new.setText("+" + NumberFormat.getInstance().format(Integer.parseInt(str_recovered_new)));
+
+                                    tv_death.setText(NumberFormat.getInstance().format(Integer.parseInt(str_death)));
+                                    tv_death_new.setText("+" + NumberFormat.getInstance().format(Integer.parseInt(str_death_new)));
+
+                                    tv_tests.setText(NumberFormat.getInstance().format(Integer.parseInt(str_tests)));
+                                    tv_tests_new.setText("+" + NumberFormat.getInstance().format(Integer.parseInt(str_tests_new)));
+
+                                    tv_date.setText(FormatDate(str_last_update_time, 1));
+                                    tv_time.setText(FormatDate(str_last_update_time, 2));
+
+
+                                    pieChart.addPieSlice(new PieModel("Active", Integer.parseInt(str_active), Color.parseColor("#007afe")));
+                                    pieChart.addPieSlice(new PieModel("Recovered", Integer.parseInt(str_recovered), Color.parseColor("#08a045")));
+                                    pieChart.addPieSlice(new PieModel("Deceased", Integer.parseInt(str_death), Color.parseColor("#F6404F")));
+
+                                    pieChart.startAnimation();
+
+                                    DismissDialog();
+                                }
+                            }, 1000);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
                     }
                 },
                 new Response.ErrorListener() {
@@ -71,9 +179,45 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+        requestQueue.add(jsonObjectRequest);
     }
 
+    public void ShowDialog(Context context) {
+        //setting up progress dialog
+        progressDialog = new ProgressDialog(context);
+        progressDialog.show();
+        progressDialog.setContentView(R.layout.progress_dialog);
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+    }
 
+    public void DismissDialog() {
+        progressDialog.dismiss();
+    }
+
+    public String FormatDate(String date, int testCase) {
+        Date mDate = null;
+        String dateFormat;
+        try {
+            mDate = new SimpleDateFormat("dd/MM/yyyy HH:mm", Locale.US).parse(date);
+            if (testCase == 0) {
+                dateFormat = new SimpleDateFormat("dd MMM yyyy, hh:mm a").format(mDate);
+                return dateFormat;
+            } else if (testCase == 1) {
+                dateFormat = new SimpleDateFormat("dd MMM yyyy").format(mDate);
+                return dateFormat;
+            } else if (testCase == 2) {
+                dateFormat = new SimpleDateFormat("hh:mm a").format(mDate);
+                return dateFormat;
+            } else {
+                Log.d("error", "Wrong input! Choose from 0 to 2");
+                return "Error";
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return date;
+        }
+    }
 
     private void init() {
         tv_confirmed = findViewById(R.id.activity_main_confirmed_textview);
@@ -92,24 +236,24 @@ public class MainActivity extends AppCompatActivity {
         pieChart = findViewById(R.id.activity_main_piechart);
         swipeRefreshLayout = findViewById(R.id.activity_main_swipe_refresh_layout);
 
+        lin_state_data = findViewById(R.id.activity_main_statewise_lin);
+        lin_world_data = findViewById(R.id.activity_main_world_data_lin);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
-        menuInflater .inflate(R.menu.menu,menu);
+        menuInflater.inflate(R.menu.menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId()==R.id.menu_about){
+        if (item.getItemId() == R.id.menu_about) {
             Toast.makeText(this, "About menu icon Clicked", Toast.LENGTH_SHORT).show();
-        }
-        else if(item.getItemId()==R.id.menu_state_wise){
+        } else if (item.getItemId() == R.id.menu_state_wise) {
             Toast.makeText(this, "StateWise Data Clicked", Toast.LENGTH_SHORT).show();
-        }
-        else if(item.getItemId()==R.id.menu_country_wise){
+        } else if (item.getItemId() == R.id.menu_country_wise) {
             Toast.makeText(this, "Country Wise Data Clicked", Toast.LENGTH_SHORT).show();
         }
         return super.onOptionsItemSelected(item);
